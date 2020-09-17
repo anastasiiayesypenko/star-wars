5764f95ce18e6e934e142091ba25386242782528
Anastasia Yesypenko <nastiyayesypenko@ukr.net>
2020-09-17 19:30:46 +0300

    Updates

---
+++ precache-manifest.f1da1049634e4967818f7143b79c0644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880966e5f751ca0949ebf80beca62c61",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "008bc954bfd6d2163610",
+    "url": "/star-wars/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "77cdf26f56f2d7a76090",
+    "url": "/star-wars/static/css/main.4743d36f.chunk.css"
+  },
+  {
+    "revision": "008bc954bfd6d2163610",
+    "url": "/star-wars/static/js/2.c81852b8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/star-wars/static/js/2.c81852b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77cdf26f56f2d7a76090",
+    "url": "/star-wars/static/js/main.c766effd.chunk.js"
+  },
+  {
+    "revision": "dbcfabd80b3cff4b6d3e",
+    "url": "/star-wars/static/js/runtime-main.6f9ba69b.js"
+  }
+]);
